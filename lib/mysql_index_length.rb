52cb5ae9b9f07f3604bc934d028cb84ecc20a1f7
Emili Parreno <emili@eparreno.com>
2009-05-27 09:18:22 +0200

    fixed problem genrerating schema.rb

--- lib/mysql_index_length.rb
+++ lib/mysql_index_length.rb
@@ -1,5 +1,48 @@
+ActiveRecord::ConnectionAdapters.send(:remove_const, :IndexDefinition)
+
 module ActiveRecord
+  class SchemaDumper #:nodoc:
+    def indexes(table, stream)
+      if (indexes = @connection.indexes(table)).any?
+        add_index_statements = indexes.map do |index|
+          statment_parts = [ ('add_index ' + index.table.inspect) ]
+          statment_parts << index.columns.inspect
+          statment_parts << (':name => ' + index.name.inspect)
+          statment_parts << ':unique => true' if index.unique
+          statment_parts << (':limit => ' + Hash[*index.columns.zip(index.limits).flatten].inspect) if index.limits
+          '  ' + statment_parts.join(', ')
+        end
+
+        stream.puts add_index_statements.sort.join("\n")
+        stream.puts
+      end
+    end
+  end
+  
   module ConnectionAdapters # :nodoc:
+    class IndexDefinition < Struct.new(:table, :name, :unique, :columns,:limits) #:nodoc:
+    end
+    
+    class MysqlAdapter < AbstractAdapter
+      def indexes(table_name, name = nil)#:nodoc:
+        indexes = []
+        current_index = nil
+        result = execute("SHOW KEYS FROM #{quote_table_name(table_name)}", name)
+        result.each do |row|
+          if current_index != row[2]
+            next if row[2] == "PRIMARY" # skip the primary key
+            current_index = row[2]
+            indexes << IndexDefinition.new(row[0], row[2], row[1] == "0", [], [])
+          end
+
+          indexes.last.columns << row[4]
+          indexes.last.limits << row[7]
+        end
+        result.free
+        indexes
+      end
+    end
+    
     module SchemaStatements
       def add_index(table_name, column_name, options = {})
         column_names = Array(column_name)
@@ -32,53 +75,7 @@ module ActiveRecord
         else
           index_name(table_name, :column => options)
         end
-      end
-      
-    end
-  end
-end
-
-module ActiveRecord
-  module ConnectionAdapters
-  class MysqlAdapter < AbstractAdapter
-    def indexes(table_name, name = nil)#:nodoc:
-      indexes = []
-      current_index = nil
-      result = execute("SHOW KEYS FROM #{quote_table_name(table_name)}", name)
-      result.each do |row|
-        if current_index != row[2]
-          next if row[2] == "PRIMARY" # skip the primary key
-          current_index = row[2]
-          indexes << IndexDefinition.new(row[0], row[2], row[1] == "0", [], [])
-        end
-
-        indexes.last.limits << row[7]
-      end
-      result.free
-      indexes
-    end
-  end
-end
-end
-
-module ActiveRecord
-  # This class is used to dump the database schema for some connection to some
-  # output format (i.e., ActiveRecord::Schema).
-  class SchemaDumper #:nodoc:
-    def indexes(table, stream)
-      if (indexes = @connection.indexes(table)).any?
-        add_index_statements = indexes.map do |index|
-          statment_parts = [ ('add_index ' + index.table.inspect) ]
-          statment_parts << index.columns.inspect
-          statment_parts << (':name => ' + index.name.inspect)
-          statment_parts << ':unique => true' if index.unique
-          statment_parts << (':limit => ' + Hash[*index.columns.zip(index.limits).flatten].inspect) if index.limits
-          '  ' + statment_parts.join(', ')
-        end
-
-        stream.puts add_index_statements.sort.join("\n")
-        stream.puts
-      end
+      end  
     end
   end
 end
